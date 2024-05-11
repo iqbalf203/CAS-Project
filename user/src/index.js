1369ed6a9a378734e40628a0337b20bbbdb1c299
iqbalf203 <iqbalf203@gmail.com>
2024-05-11 21:13:20 +0530

    commit

--- user/src/index.js
+++ user/src/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(authenticateJWT); 
 
-const PORT = process.env.PORT || 2000; // set PORT=2002 && npm start
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
